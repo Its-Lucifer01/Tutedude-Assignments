f258953ba2f34106e55bd3d82846db152304aba8
Srishti Kaushik <kaushiksrishti108@gmail.com>
2025-09-11 22:37:34 +0530

    First Assignment Solution

---
+++ Assignment-1.py
@@ -0,0 +1,19 @@
+
+# Task 1: Perform Basic Mathematical Operations
+print("=== Calculator === ")
+A = int(input("Enter the First number: "))
+B = int(input("Enter the Second number: "))
+
+print("Addition: ", A+B)
+print("Subtraction: ", A-B)
+print("Multiplication: ", A*B)
+print("Division: ", A/B)
+
+
+# Task 2: Create a Personalized Greeting
+print("=== Greet the User === ")
+Fname = input(" Enter Your First Name here: ")
+Lname = input(" Enter Your Last Name here: ")
+
+print("Hello,", Fname, Lname, "!, Welcome to the Python Program !!")
+
